f5e22ce593274f3e0e2228cb36216e0aa8ec8ce3
CTA <cta@CTAs-MacBook-Pro.local>
2023-04-13 10:24:53 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Da first Gatsby website`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
