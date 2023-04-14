5c6399b1d70ecbfdf45e69625eeafc8584146188
CTA <cta@CTAs-MacBook-Pro.local>
2023-04-14 11:48:27 -0700

    see if this works

---
+++ src/pages/about2.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+const AboutPage2 = () => {
+    return (
+        <main>
+            <h1>About Cancel This Agency</h1>
+            <p>Cancel This Agency is more ethos than anything else, although it is a collective of designers creating the next generation of useful products through an organized process that removes more than it adds, but for good.</p>
+            <p>We use the term "agency" in two ways. It is indeed a company &lpar;uniquely&rpar;, but it is also a way to feel permission to do something.</p>
+        </main>
+    )
+}
+
+export const Head = () => <title>About Cancel This Agency</title>
+
+export default AboutPage2
---
+++ src/pages/why.js
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+const Why = () => {
+    return (
+        <main>
+            Oh hi.
+        </main>
+    )
+}
